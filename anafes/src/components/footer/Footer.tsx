7469ebadbaa8fb58a3ad66a08457e3261e75ce4a
mahmoud-cr7 <you@example.comwageeh@DESKTOP-OLGM1G0>
2024-09-23 23:19:17 +0300

    add photos and make footer and navbar

---
+++ anafes/src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="social">
+        <img src="/src/assets/icons/facebook.png" alt="facebook" />
+        <img src="/src/assets/icons/insataa.png" alt="insataa" />
+        <img src="/src/assets/icons/twitter.png" alt="twitter" />
+      </div>
+      <p>جميع الحقوق محفوظة - نافس 2022</p>
+      <div>
+        <img src="/src/assets/images/logo.png" alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
